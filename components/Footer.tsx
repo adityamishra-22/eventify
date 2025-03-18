fdfc0ff2a0fe1e925912743497203036860a45f1
adityamishra22 <adi22mishra@gmail.com>
2025-03-19 00:34:23 +0530

    new update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import SocialMedia from './SocialMedia';
+import React from "react";
+import SocialMedia from "./SocialMedia";
 
 const Footer = () => {
   return (
     <div className="h-[20rem] flex flex-col justify-center items-center text-center border-t-[0.1rem] border-gray-100 ">
       <div className="flex flex-col gap-4 items-center">
-        <img  src="/mammothyLogo.png" alt="Mammothy Logo" className="" />
-        <p className="text-center font-inter text-base font-normal leading-6 text-[#6B6B6B]
-">
-          Marketplace for searching, filtering, and instantly booking team activities
+        <img src="/thelogo.png" alt="Mammothy Logo" className="" />
+        <p
+          className="text-center font-inter text-base font-normal leading-6 text-[#6B6B6B]
+"
+        >
+          Marketplace for searching, filtering, and instantly booking team
+          activities
         </p>
         <SocialMedia />
-        <div className=' w-[70rem] border-[0.1rem] border-gray-100'>       
-        </div>
-        <div className='text-[#6B6B6B]'>
-            Copyright © 2024
-        </div>
+        <div className=" w-[70rem] border-[0.1rem] border-gray-100"></div>
+        <div className="text-[#6B6B6B]">Copyright © 2025</div>
       </div>
     </div>
   );
--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
+import React from "react";
 import { FaRegCircleUser } from "react-icons/fa6";
 
 const MainHeader = () => {
   return (
-    <div className='flex justify-between align-middle content-center px-[5rem] py-[1rem] border-b-[0.1rem] border-gray-300'>
-      <img src="/mammothyLogo.png" alt="" />
-      <div className='flex items-center gap-3'>
+    <div className="flex justify-between align-middle content-center px-[5rem] py-[1rem] border-b-[0.1rem] border-gray-300">
+      <img src="/thelogo.png" alt="" />
+      <div className="flex items-center gap-3">
         <img src="/Avatar.png" alt="" />
-      <div>
-        Profile
-      </div>
+        <div>Profile</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainHeader
+export default MainHeader;
